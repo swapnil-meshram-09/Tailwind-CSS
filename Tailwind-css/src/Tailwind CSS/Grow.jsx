1e077bc36d69e6a1c85ad36e7cce2e65bd649ff3
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:47:07 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Grow.jsx
@@ -0,0 +1,31 @@
+
+function Grow() {
+  return (
+    <>
+     {/* <div className='bg-amber-500 flex'>
+        <div className='grow-10'>A</div>
+        <div className='grow-10'>B</div>
+        <div className='grow-10'>C</div>
+        <div className='grow-10'>D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex'>
+        <div className='grow-10'>A</div>
+        <div className='flex-none'>B</div>
+        <div className='grow-10'>C</div>
+        <div className='flex-none'>D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex'>
+        <div className='size-10 grow-10'>A</div>
+        <div className='size-10 grow-10'>B</div>
+        {/* <div className='size-0 grow-0'>B</div>  => overlapping eachother 
+        <div className='size-10 grow-10'>C</div>
+        <div className='size-10 grow-10'>D</div>
+    </div> */}
+    
+    </>
+  )
+}
+
+export default Grow
