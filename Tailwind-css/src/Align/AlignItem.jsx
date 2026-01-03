d1ade0e38dff7e43c2c42494a5fbdd4723b206ed
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:41:47 +0530

    new commit

---
+++ Tailwind-css/src/Align/AlignItem.jsx
@@ -0,0 +1,39 @@
+
+function AlignItem() {
+  return (
+    <>
+    {/* <div className="bg-blue-900 flex items-start">
+       <div className="p-1 bg-blue-300">A</div>
+       <div className="p-5 bg-blue-300">B</div>
+       <div className="p-1 bg-blue-300">C</div>
+    </div> */}
+
+    {/* <div className="bg-blue-900 flex items-center">
+       <div className="p-1 bg-blue-300">A</div>
+       <div className="p-5 bg-blue-300">B</div>
+       <div className="p-1 bg-blue-300">C</div>
+    </div> */}
+
+    {/* <div className="bg-blue-900 flex items-end">
+       <div className="p-1 bg-blue-300">A</div>
+       <div className="p-5 bg-blue-300">B</div>
+       <div className="p-1 bg-blue-300">C</div>
+    </div> */}
+{/* 
+    <div className="bg-blue-900 flex items-baseline">
+       <div className="pt-2 pb-6 bg-blue-300">A</div>
+       <div className="pt-8 pb-12 bg-blue-300">B</div>
+       <div className="pt-12 pb-4 bg-blue-300">C</div>
+    </div> */}
+
+    {/* <div className="bg-blue-900 flex items-stretch">
+       <div className="p-1 bg-blue-300">A</div>
+       <div className="p-5 bg-blue-300">B</div>
+       <div className="p-1 bg-blue-300">C</div>
+    </div> */}
+
+    </>
+  )
+}
+
+export default AlignItem
