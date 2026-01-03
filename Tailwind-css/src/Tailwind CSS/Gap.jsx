c8c57ff8bd8001ffa8f12cdd49f75389ba168457
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:45:13 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Gap.jsx
@@ -0,0 +1,37 @@
+
+export default function Gap() {
+  return (
+    <>
+
+    {/* <div className='bg-amber-500 flex gap-10'>
+        <div className="">A</div>
+        <div className="">B</div>
+        <div className="">C</div>
+        <div className="">D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex flex-col gap-20'>
+        <div className="">A</div>
+        <div className="">B</div>
+        <div className="">C</div>
+        <div className="">D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 grid gap-20'>
+        <div className="">A</div>
+        <div className="">B</div>
+        <div className="">C</div>
+        <div className="">D</div>
+    </div> */}
+
+    
+     {/* <div className='bg-amber-500 grid grid-cols-3 gap-10'>
+        <div className="">A</div>
+        <div className="">B</div>
+        <div className="">C</div>
+        <div className="">D</div>
+    </div> */}
+    
+    </>
+  )
+}
