25a45e3e9e62935c41ec9e7c5e2c31d0a9d3f52f
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:40:39 +0530

    new commit

---
+++ Tailwind-css/src/App.jsx
@@ -0,0 +1,49 @@
+import TailwindCss from './Tailwind CSS/TailwindCss'
+import Flex from './Tailwind CSS/Flex/Flex'
+import FlexWrap from './Tailwind CSS/Flex/FlexWrap'
+import Grow from './Tailwind CSS/Grow'
+import Shrink from './Tailwind CSS/Shrink'
+import Gap from './Tailwind CSS/Gap'
+import Padding from './Tailwind CSS/Padding'
+import Margin from './Tailwind CSS/Margin'
+import Zindex from './Tailwind CSS/Zindex'
+import Opacity from './Tailwind CSS/Opacity'
+import AlignItem from './Align/AlignItem'
+import JustifyItems from './Justify/JustifyItems'
+
+
+function App() {  
+
+  return (
+    <>
+
+    <TailwindCss />
+
+    <Flex />
+
+    <FlexWrap />
+
+    <Grow />
+
+    <Shrink />
+
+    <Gap />
+
+    <Padding />
+
+    <Margin />
+
+    <Zindex />
+
+    <Opacity />
+
+    <AlignItem />
+
+    <JustifyItems />
+
+
+    </>
+  )
+}
+
+export default App
