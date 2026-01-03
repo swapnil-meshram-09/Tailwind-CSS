31786c071e5b46e5d45685080f388ddc9a519f79
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:43:38 +0530

    new commit

---
+++ Tailwind-css/src/Flex/Flex.jsx
@@ -0,0 +1,55 @@
+
+
+function Flex() {
+  return (
+    <>
+    
+    {/* <div className='bg-amber-500 flex'>
+        <div className='basis-64'>A</div>
+        <div className='basis-64'>B</div>
+        <div className='basis-64'>C</div>
+        <div className='basis-sm'>D</div>
+    </div> */}
+
+     
+    {/* <div className='bg-amber-500 flex'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+     {/* <div className='bg-amber-500 flex flex-row'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+     {/* <div className='bg-amber-500 flex flex-col'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex flex-row-reverse'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex flex-col-reverse'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+
+    </>
+  )
+}
+
+export default Flex
