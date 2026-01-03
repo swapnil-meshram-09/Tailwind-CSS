6a47788ae21a057679c9285d233716601280ea25
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-03 20:42:34 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/TailwindCss.jsx
@@ -0,0 +1,61 @@
+function TailwindCss() {
+  return (
+    <>
+    {/* // <div className='bg-black relative m-10'> */}
+      
+        {/* <h1 className='bg-amber-500'>Background Color</h1>
+
+        <h1 className='text-green-500'>Text Color</h1>
+
+
+        <h1 className='text-2xl'>Text Size</h1>
+
+        <h1 className='text-center'>Text Center</h1>
+
+        <h1 className='text-right'>Text Right</h1>
+
+        <h1 className='text-shadow-2xs'>Text Shadow</h1>
+        
+
+        <h1 className='bg-amber-500 px-15'>Padding in X</h1>
+
+        <h1 className='bg-amber-100 py-3'>Padding in Y</h1>
+
+        <h1 className='bg-amber-500 p-5'>Padding</h1>
+
+
+        <h1 className='border-1'>Border</h1>
+
+        <h1 className='border-1 border-red-500'>Border with Color</h1>
+
+
+        <h1 className='bg-amber-500 m-5'>Margin</h1>
+
+        <h1 className='bg-amber-200 mx-25'>Margin</h1>
+
+        <h1 className='bg-amber-500 my-3'>Margin</h1>
+
+        <h1 className='bg-red-300 w-12'>Width</h1>
+
+        <h1 className='bg-red-500 h-20'>Height</h1>
+
+        <h1 className='bg-yellow-300 font-light'>Font Light</h1>
+
+        <button className='bg-green-500 rounded-xl px-1 mx-29 my-1 hover:bg-blue-400 hover:text-white'>Submit</button>
+    */} 
+    {/* <div className="bg-red-400 flex m-1 p-10 gap-30 absolute ">
+
+    <h1 className='bg-amber-500 text-white text-2xl '>Heading</h1>
+    <h1 className='bg-amber-500 text-white text-2xl'>Heading</h1>
+    <h1 className='bg-amber-500 text-white text-2xl'>Heading</h1>
+    <h1 className='bg-amber-500 text-white text-2xl'>Heading</h1>
+    <h1 className='transform translate-x-24'>Heading</h1>
+    
+    </div> */}
+
+    {/* // </div> */}
+    </>
+  )
+}
+
+export default TailwindCss
