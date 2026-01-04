b221e9cdac4f91f6868e4fcccdb8e4ec5ee50c7b
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:34:52 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Justify/Justify.jsx
@@ -0,0 +1,65 @@
+
+function Justify() {
+  return (
+    <>
+    {/* <div className='flex justify-start'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='flex justify-center'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='flex justify-end'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='flex justify-between'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+{/* 
+    <div className='flex justify-around'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='flex justify-evenly'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+
+    {/* <div className='flex justify-stretch'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+{/* 
+    <div className='flex justify-baseline'>
+      <div className=''>A</div>
+      <div className=''>B</div>
+      <div className=''>C</div>
+      <div className=''>D</div>
+    </div> */}
+    
+    </>
+  )
+}
+
+export default Justify
