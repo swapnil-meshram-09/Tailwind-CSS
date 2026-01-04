fcc6cc98d9b49b028910f9ca521b4563880f7b67
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:35:44 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Padding.jsx
@@ -0,0 +1,31 @@
+
+function Padding() {
+  return (
+    <>
+      {/* <div className="p-10 bg-amber-300">A</div>
+
+      <div className="pt-5 bg-amber-500">B</div>
+      <div className="pr-10 bg-amber-900">C</div>
+      <div className="pl-10 bg-amber-300">D</div>
+      <div className="pb-20 bg-amber-800">E</div>
+
+
+      <div className="px-50 bg-amber-500">F</div>
+      <div className="py-20 bg-amber-900">G</div>   */}
+
+      {/* <div>
+        <div dir="ltr">
+            <div className="ps-10 bg-amber-200">A</div>
+            <div className="pe-10 bg-amber-900">B</div>
+        </div>
+        <div dir="rtl">
+            <div className="ps-10 bg-amber-200">C</div>
+            <div className="pe-10 bg-amber-900">D</div>
+        </div>
+      </div> */}
+
+     </>
+  )
+}
+
+export default Padding
