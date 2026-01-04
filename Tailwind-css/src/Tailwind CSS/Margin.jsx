7e870227bfcbd5c1f49c077ea80dd8891876c524
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:35:19 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Margin.jsx
@@ -0,0 +1,21 @@
+
+function Margin() {
+  return (
+     <>
+     {/* <div className="m-3 bg-red-500">A</div> */}
+
+     {/* <div className="mt-10 bg-red-500">B</div>
+     <div className="mr-150 bg-red-500">C</div>
+     <div className="mb-10 bg-red-500">D</div>
+     <div className="ml-50 bg-red-500">D</div> */}
+{/* 
+     <div className="mx-80 bg-red-500">E</div>
+     <div className="my-10 bg-red-500">F</div> */}
+
+
+     
+     </>
+  )
+}
+
+export default Margin
