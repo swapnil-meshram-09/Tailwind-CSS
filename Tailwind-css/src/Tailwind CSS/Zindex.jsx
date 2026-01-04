0236a33a383bdebeac737dca834e42c1c61f93de
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:36:14 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Zindex.jsx
@@ -0,0 +1,20 @@
+
+function Zindex() {
+  return (
+    <>
+    {/* <div className="flex relative">
+    <div className='z-1 absolute bg-green-400'>A</div>
+    <div className='z-10 absolute bg-green-400'>B</div>  
+    <div className='-z-1 absolute bg-green-400'>c</div>  
+    </div> */}
+{/* 
+    <div className="flex relative">
+    <div className='z-10 absolute bg-green-400'>A</div>
+    <div className='z-auto absolute bg-green-400'>B</div>  
+    <div className='absolute bg-green-400'>c</div>  
+    </div> */}
+    </>
+  )
+}
+
+export default Zindex
