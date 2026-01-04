a9bbb1a1b43bb6316e032e0119279924429d1583
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:38:02 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Postion/Static.jsx
@@ -0,0 +1,46 @@
+
+function Static() {
+  return (
+    <>
+     {/* <div className='static flex justify-evenly bg-amber-300'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+
+        <div className='absolute top-50 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='absolute top-100 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='absolute top-150 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='absolute top-100 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+        
+     </div> */}
+
+
+    </>
+  )
+}
+
+export default Static
