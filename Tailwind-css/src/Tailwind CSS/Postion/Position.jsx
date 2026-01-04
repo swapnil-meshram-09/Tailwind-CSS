f142e5cfae62e296ddf624f62cc0fad03b866113
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:38:36 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Postion/Position.jsx
@@ -0,0 +1,28 @@
+
+function Position() {
+  return (
+    <>
+    {/* <div className='relative left-50 top-35 h-6 w-20 bg-green-500'>
+        <p className='absolute top-0 left-0 p-0 bg-amber-300'>this is para.</p>
+    </div> 
+    <div className='relative left-100 top-60 h-6 w-20 bg-green-500'>
+        <p className='absolute left-0 top-0 bg-amber-300'>this is para.</p>
+    </div>  */}
+
+
+    {/* <div className='absolute top-100 bg-amber-400'>Top</div>
+    <div className='absolute left-150 bg-amber-400'>Left</div>
+    <div className='absolute right-150 bg-amber-400'>Right</div>
+    <div className='absolute bottom-100 bg-amber-400'>Bottom</div> */}
+
+    {/* <div className='absolute -top-3 bg-amber-400'>-Top</div>
+    <div className='absolute -left-3 top-10 bg-amber-400'>-Left</div>
+    <div className='absolute -right-3 bg-amber-400'>-Right</div>
+    <div className='absolute -bottom-200 bg-amber-400'>-Bottom</div>
+    
+     */}
+    </>
+  )
+}
+
+export default Position
