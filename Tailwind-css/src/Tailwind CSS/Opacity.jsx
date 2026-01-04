e64e7f3dc200f3496523afaf26751b97cb6ef176
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:35:33 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Opacity.jsx
@@ -0,0 +1,15 @@
+function Opacity() {
+  return (
+    <>
+    {/* <div className="bg-green-400 flex gap-2">
+    <button className="opacity-0 hover:opacity-100 hover:bg-amber-400 bg-blue-400 rounded-4xl">A</button>
+    <button className="opacity-5 bg-blue-400 rounded-4xl">B</button>
+    <button className="opacity-50 bg-blue-400 rounded-4xl">C</button>
+    <button className="opacity-100 bg-blue-400 rounded-4xl">D</button>
+    
+    </div> */}
+    </>
+  )
+}
+
+export default Opacity
