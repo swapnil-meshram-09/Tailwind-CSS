296764dc895580640c1fa59d3a6b77f5c7b3c79e
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:38:44 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Postion/Fixed.jsx
@@ -0,0 +1,151 @@
+
+function Fixed() {
+  return (
+    <>
+      <div className='relative bg-amber-300'>
+        <div className='fixed flex z-10 top-0 left-0 right-0 justify-center bg-green-300'>
+            <div className=''>A</div>
+           <div className=''>B</div>
+           <div className=''>C</div>
+           <div className=''>D</div>
+        </div>
+
+        <div className='absolute top-10'>
+
+        <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className=' flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className=' flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className=' flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         </div>
+        
+     </div>
+    
+    </>
+  )
+}
+
+export default Fixed
