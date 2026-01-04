c9100cf6389e0e627aa020c0078ec21525d46993
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:34:09 +0530

    new commit

--- Tailwind-css/src/App.jsx
+++ Tailwind-css/src/App.jsx
@@ -1,23 +1,27 @@
-import TailwindCss from './Tailwind CSS/TailwindCss'
-import Flex from './Tailwind CSS/Flex/Flex'
-import FlexWrap from './Tailwind CSS/Flex/FlexWrap'
-import Grow from './Tailwind CSS/Grow'
-import Shrink from './Tailwind CSS/Shrink'
-import Gap from './Tailwind CSS/Gap'
-import Padding from './Tailwind CSS/Padding'
-import Margin from './Tailwind CSS/Margin'
-import Zindex from './Tailwind CSS/Zindex'
-import Opacity from './Tailwind CSS/Opacity'
-import AlignItem from './Align/AlignItem'
-import JustifyItems from './Justify/JustifyItems'
-
+// import TailwindCss from './Tailwind CSS/TailwindCss'
+// import Flex from './Tailwind CSS/Flex/Flex'
+// import FlexWrap from './Tailwind CSS/Flex/FlexWrap'
+// import Grow from './Tailwind CSS/Grow'
+// import Shrink from './Tailwind CSS/Shrink'
+// import Gap from './Tailwind CSS/Gap'
+// import Padding from './Tailwind CSS/Padding'
+// import Margin from './Tailwind CSS/Margin'
+// import Zindex from './Tailwind CSS/Zindex'
+// import Opacity from './Tailwind CSS/Opacity'
+// import AlignItem from './Align/AlignItem'
+// import Justify from "./Tailwind CSS/Justify/Justify"
+// import JustifyItems from "./Tailwind CSS/Justify/JustifyItems"
+// import Static from './Tailwind CSS/Postion/Static'
+// import Relative from './Tailwind CSS/Postion/Relative'
+// import Fixed from './Tailwind CSS/Postion/Fixed'
+import Position from "./Tailwind CSS/Postion/Position"
 
 function App() {  
 
   return (
     <>
 
-    <TailwindCss />
+    {/* <TailwindCss />
 
     <Flex />
 
@@ -37,9 +41,22 @@ function App() {
 
     <Opacity />
 
-    <AlignItem />
+    <AlignItem /> 
+
+    <Justify />
 
     <JustifyItems />
+
+    <Static />
+
+    <Relative />
+
+    <Fixed /> 
+
+    <Position />
+
+    */}
+
 
 
     </>
