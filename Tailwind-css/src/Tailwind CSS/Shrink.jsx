9d0ccf7c35e82170d58ef86c64cf9256b46057cd
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:37:20 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Shrink.jsx
@@ -0,0 +1,27 @@
+
+function Shrink() {
+  return (
+    <>
+
+    {/* <div className='bg-amber-500 flex'>
+        <div className='size-10 shrink'>A</div>
+        <div className='size-10 shrink'>B</div>
+        <div className='size-10 shrink'>C</div>
+        <div className='size-10 shrink'>D</div>
+    </div> */}
+
+    {/* <div className='bg-amber-500 flex'>
+        <div className='h-6 w-10 flex-1'>A</div>
+        <div className='h-6 w-10 shrink'>B</div>
+        <div className='h-6 w-10 flex-10'>C</div>
+        <div className='h-6 w-10 shrink'>D</div>
+    </div> */}
+
+   
+    
+    
+    </>
+  )
+}
+
+export default Shrink
