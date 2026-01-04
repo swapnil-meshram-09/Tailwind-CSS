e0501dc7f7dce721c9551679b71cb758d8780693
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:35:04 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Justify/JustifyItems.jsx
@@ -0,0 +1,36 @@
+
+function JustifyItems() {
+  return (
+    <>
+    {/* <div className='grid justify-items-start'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+     {/* <div className='grid justify-items-center'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+     {/* <div className='grid justify-items-end'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+
+     {/* <div className='grid justify-items-stretch'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+    </div> */}
+    </>
+  )
+}
+
+export default JustifyItems
