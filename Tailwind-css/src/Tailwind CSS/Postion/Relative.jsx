b8f71e13955fe5cf7d5370cda3616a79d271c99e
swapnil-meshram-09 <swapnilmeshram514@gmail.com>
2026-01-04 20:38:27 +0530

    new commit

---
+++ Tailwind-css/src/Tailwind CSS/Postion/Relative.jsx
@@ -0,0 +1,46 @@
+
+function Relative() {
+  return (
+    <>
+     {/* <div className='relative flex justify-evenly bg-amber-300'>
+        <div className=''>A</div>
+        <div className=''>B</div>
+        <div className=''>C</div>
+        <div className=''>D</div>
+
+        <div className='absolute left-0 top-50 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='absolute left-0 top-100 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+          <div className='absolute left-0 top-150 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+
+         <div className='absolute left-0 top-100 flex justify-evenly bg-amber-300'>
+             <div className=''>A</div>
+             <div className=''>B</div>
+             <div className=''>C</div>
+             <div className=''>D</div>
+         </div>
+        
+     </div> */}
+
+    
+    </>
+  )
+}
+
+export default Relative
